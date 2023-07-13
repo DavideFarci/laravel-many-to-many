a983e965d9921ee03349e56cae11f63d3009a75f
DavideFarci <yank92.df@gmail.com>
2023-07-13 16:16:56 +0200

    add column in table projects for image

--- database/migrations/2023_07_06_134942_create_projects_table.php
+++ database/migrations/2023_07_06_134942_create_projects_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->string('collaborators', 150)->nullable();
             $table->text('description')->nullable();
             $table->string('link_github', 150);
+            $table->string('image', 200)->nullable();
 
             $table->softDeletes();
             // $table->timestamps();
